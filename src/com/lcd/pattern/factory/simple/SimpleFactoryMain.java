066afac3de620db4312d12499444b3435b01d59e
linchengdian <linchengdian@irissz.com>
2020-01-20 14:44:34 +0800

    修改一下文件名字

--- src/com/lcd/pattern/factory/simple/SimpleFactoryMain.java
+++ src/com/lcd/pattern/factory/simple/SimpleFactoryMain.java
@@ -4,7 +4,7 @@ package com.lcd.pattern.factory.simple;
  * 简单工厂模式小案例
  * 缺点：不好拓展，不符合开闭原则
  */
-public class FactoryTest {
+public class SimpleFactoryMain {
     public static void main(String[] args) {
 
         Factory factory = new Factory();
