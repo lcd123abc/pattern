eb9b0b98ec815f8898cacc974874c9d2988c3535
linchengdian <linchengdian@irissz.com>
2020-01-21 11:07:39 +0800

    单例模式-懒汉式改进版

---
+++ src/com/lcd/pattern/singleton/LazySingletonTwo.java
@@ -0,0 +1,20 @@
+package com.lcd.pattern.singleton;
+
+/**
+ * 懒汉模式改进版，加上锁，解决了线程安全问题
+ */
+public class LazySingletonTwo {
+    private LazySingletonTwo(){ }
+    private static volatile LazySingletonTwo SINGLETON;
+
+    public static LazySingletonTwo getInstance(){
+        if(SINGLETON == null){
+            synchronized (LazySingletonTwo.class){
+                if(SINGLETON == null){
+                    SINGLETON = new LazySingletonTwo();
+                }
+            }
+        }
+        return SINGLETON;
+    }
+}
