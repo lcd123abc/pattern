bfe82e2d1fbf30f0fd530e8f65e369b89845c4a8
linchengdian <linchengdian@irissz.com>
2020-01-21 11:22:46 +0800

    单例模式-readResolve方法保证反序列化后是同一个实例

--- src/com/lcd/pattern/singleton/HungrySingleton.java
+++ src/com/lcd/pattern/singleton/HungrySingleton.java
@@ -1,6 +1,8 @@
 package com.lcd.pattern.singleton;
 
 
+import java.io.ObjectStreamException;
+
 /**
  * 单例模式-饿汉式
  * 没有线程安全问题，一开始就创建，容易产生垃圾
@@ -14,6 +16,15 @@ public class HungrySingleton {
      * 提供一个可以返回实例的方法
      */
     public static HungrySingleton getInstance(){
+        return SINGLETON;
+    }
+
+    /**
+     * 实现此方法，保证序列化后，反序列化的对象是同一个对象
+     * @return
+     * @throws ObjectStreamException
+     */
+    private Object readResolve() throws ObjectStreamException {
         return SINGLETON;
     }
 }
