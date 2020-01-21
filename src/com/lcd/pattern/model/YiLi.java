7dd18681af1f564bd6eaad879873b787f712a769
linchengdian <linchengdian@irissz.com>
2020-01-21 11:48:57 +0800

    原型模式-例子

--- src/com/lcd/pattern/model/YiLi.java
+++ src/com/lcd/pattern/model/YiLi.java
@@ -1,8 +1,14 @@
 package com.lcd.pattern.model;
 
-public class YiLi implements Mike {
+public class YiLi implements Mike,Cloneable {
     @Override
     public String getName() {
         return "伊利";
     }
+
+    @Override
+    public Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+
 }
---
+++ src/com/lcd/pattern/prototype/PrototypeMain.java
@@ -0,0 +1,23 @@
+package com.lcd.pattern.prototype;
+
+import com.lcd.pattern.model.YiLi;
+
+import javax.sound.midi.Soundbank;
+import javax.xml.transform.Source;
+
+/**
+ * 原型模式在java最常见的实现就是克隆对象
+ * 或者是使用BeanUtils去拷贝对象
+ */
+public class PrototypeMain {
+    public static void main(String[] args) {
+        YiLi yiLi = new YiLi();
+        try {
+            YiLi yiLiClone = (YiLi)yiLi.clone();
+            System.out.println(yiLi);
+            System.out.println(yiLiClone);
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
+    }
+}
