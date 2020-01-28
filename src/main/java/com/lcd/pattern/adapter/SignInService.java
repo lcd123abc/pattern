58ab503b02a89f57f87cdab8023528b195910f57
username <useremail@163.com>
2020-01-28 18:09:25 +0800

    适配模式例子

---
+++ src/main/java/com/lcd/pattern/adapter/SignInService.java
@@ -0,0 +1,14 @@
+package com.lcd.pattern.adapter;
+
+public class SignInService {
+    public ResultMsg signIn(String username,String password){
+        //注册的逻辑
+        //...
+        return new ResultMsg(200,"注册成功",null);
+    }
+    public ResultMsg login(String username,String password){
+        //登录的逻辑
+        //...
+        return new ResultMsg(200,"登录成功",null);
+    }
+}
