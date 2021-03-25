6ca8cfdc07e9d56fbe6a3ebd9a56ca857fb6a1ff
MrTangerine <573232454@qq.com>
2021-03-25 21:30:24 +0800

    volatile 单例保证线程安全

--- src/main/java/com/lcd/pattern/singleton/LazySingletonTwo.java
+++ src/main/java/com/lcd/pattern/singleton/LazySingletonTwo.java
@@ -6,6 +6,11 @@ package com.lcd.pattern.singleton;
 public class LazySingletonTwo {
     private LazySingletonTwo(){}
 
+    /**
+     * 为什么要加volatile，编译器优化代码会进行指令重排，
+     * 那么有极低的概率会发生这种情况：一个线程进入new 过程，进行了内存分配但是还没初始化
+     * 第二个线程进行判断，发现有内存地址不为null,直接返回，但是对象还没初始化，所以实际还是null
+     */
     public static volatile LazySingletonTwo SINGLETON;
 
     public static LazySingletonTwo getInstance(){
